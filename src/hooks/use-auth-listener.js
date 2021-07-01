5e5335e98ee163325beda8572b5832e90e7ebbc2
Alex Bel <council.belokon1988@gmail.com>
2021-07-01 18:22:07 +0300

    user photo added

--- src/hooks/use-auth-listener.js
+++ src/hooks/use-auth-listener.js
@@ -1,21 +1,25 @@
-import { tSImportEqualsDeclaration } from '@babel/types';
 import { useState, useEffect, useContext } from 'react';
-import FirebaseContext from '../context/firebase'
+import FirebaseContext from '../context/firebase';
 
-export defaul function useAuthListener(){
-    const [user, setUser] = useState(JSON.parse(localStorage.getItem('authUser')));
-    const { firebase } = useContext(FirebaseContext);
+export default function useAuthListener() {
+  const [user, setUser] = useState(JSON.parse(localStorage.getItem('authUser')));
+  const { firebase } = useContext(FirebaseContext);
 
-    useEffect(() =>{
-        const listener = firebase.auth().onAuthStateChanges((authUser)) => {
-            if (suthUser) {
-                localStorage.setItem('authUSer', JSON.stringify(authUSer));
-                setUser(authUser);
-            } else {
-                localStorage.removeItem('authUser');
-                setUser(null);
-            }
-        }
-    }, []);
-    return [user]
+  useEffect(() => {
+    const listener = firebase.auth().onAuthStateChanged((authUser) => {
+      if (authUser) {
+        // we have a user...therefore we can store the user in localstorage
+        localStorage.setItem('authUser', JSON.stringify(authUser));
+        setUser(authUser);
+      } else {
+        // we don't have an authUser, therefore clear the localstorage
+        localStorage.removeItem('authUser');
+        setUser(null);
+      }
+    });
+
+    return () => listener();
+  }, [firebase]);
+
+  return { user };
 }
